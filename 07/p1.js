516eb82e0517c965ebb411bffd50e4267065fda0
David McGrath <david.mcgrath13@gmail.com>
2021-12-08 09:41:56 +0800

    Update p1.js
    Added a note about the solution

--- 07/p1.js
+++ 07/p1.js
@@ -16,9 +16,9 @@ await getInput(false)
 		// First, pre-process to get limits & initialise result
 		var max = input.reduce((acc, x) => acc < x ? x : acc);
 		var result = [];
-        for (var i = 0; i < max; i++) {
-            result[i] = 0;
-        }
+		for (var i = 0; i < max; i++) {
+		    result[i] = 0;
+		}
 		
 		// Iterate through all values, populate array with number of moves to reach that position
 		// Sum these together, take the minimum
@@ -29,5 +29,7 @@ await getInput(false)
 			}
 		}
 		
+		// This just results in the median position. Realistically only one position needs to be checked.
+		// Naïve solution is suitably fast though. 
 		return result.reduce((acc, x) => acc < x ? acc : x);
     });
