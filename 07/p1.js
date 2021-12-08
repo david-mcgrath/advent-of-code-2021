927ccb8dff6125e5ab0d5a93fbfd16997c161668
David McGrath <david.mcgrath13@gmail.com>
2021-12-08 09:34:57 +0800

    Added day 7
    Added solution to day 7, pretty easy one but the partial sum is kind of fun.

---
+++ 07/p1.js
@@ -0,0 +1,33 @@
+var url = "https://adventofcode.com/2021/day/7/input";
+var testData = `16,1,2,0,4,2,7,1,2,14`;
+
+function getInput(isTest) {
+    return isTest
+        ? new Promise((resolve, reject) => resolve(testData))
+        : fetch(url).then(res => res.text());
+}
+
+await getInput(false)
+    .then(input => {
+        input = input
+            .split(",")
+            .map(x => +x);
+
+		// First, pre-process to get limits & initialise result
+		var max = input.reduce((acc, x) => acc < x ? x : acc);
+		var result = [];
+        for (var i = 0; i < max; i++) {
+            result[i] = 0;
+        }
+		
+		// Iterate through all values, populate array with number of moves to reach that position
+		// Sum these together, take the minimum
+		for (var i = 0; i < input.length; i++) {
+			var value = input[i];
+			for (var j = 0; j < max; j++) {
+				result[j] += Math.abs(value - j);
+			}
+		}
+		
+		return result.reduce((acc, x) => acc < x ? acc : x);
+    });
